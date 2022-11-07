e1bffb8cc6c6c14ee302a95c53ec3697301538c8
GhostrykerII <tyler_kasper@hotmail.com>
2022-11-07 10:45:08 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean
+{
+    internal class Program
+
+    {
+        static bool gameOver; //true or false
+        static int x;
+        static int y;
+        static void Main(string[] args)
+        {
+
+
+
+            x = 10;
+            y = 10;
+
+            //game loop
+            while(gameOver == false)
+            {
+                //update
+                PlayerUpdate();
+                PlayerDraw();
+
+                //draw
+
+                //debug
+               // Console.Write(x);
+               // Console.Write(y);
+               // Console.ReadKey();
+                
+            }
+            Console.Clear();
+            Console.WriteLine("Game Over");
+            Console.ReadKey(true);
+        }
+
+        static void PlayerUpdate()
+        {
+            //READ USER INPUT
+
+            ConsoleKeyInfo input;
+            input = Console.ReadKey(true);
+
+            //Console.WriteLine(input);
+           // Console.WriteLine(input.KeyChar);  // returns character
+           // Console.WriteLine(input.Key); // return key being pressed
+
+            //UPDATE PLAYER POSITION
+
+            if(input.KeyChar == 'w')
+            {
+
+
+                y = y - 1; // 
+
+
+            }
+
+            if (input.KeyChar == 'a')
+            {
+                x = x - 1; // 
+            }
+
+            if (input.KeyChar == 's')
+            {
+                y = y + 1; // 
+            }
+
+            if (input.KeyChar == 'd')
+            {
+                x = x + 1; // 
+            }
+
+            if (input.Key == ConsoleKey.Escape)
+            {
+                gameOver = true;   
+            }
+
+            //DEBUG
+           // Console.WriteLine("(" + x + "," + y + ")");
+
+        }
+
+        static void PlayerDraw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write("#");
+        }
+
+
+        }
+    }
+
